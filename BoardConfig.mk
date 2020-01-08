be62f147633d7d196adc9023777f726330e6fe57
skyrmion <skyrmion@live.com>
2020-01-07 19:01:04 -0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,11 +47,14 @@ BOARD_DTBTOOL_ARGS := --force-v2
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := zImage
+TARGET_KERNEL_SOURCE := kernel/sharp/msm8226
+TARGET_KERNEL_CONFIG := pb25_defconfig
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_ROOT_EXTRA_FOLDERS := firmware fsg persist
 LZMA_RAMDISK_TARGETS := recovery
+
 TARGET_KERNEL_SOURCE := TARGET_PREBUILT_KERNEL := device/sharp/sg306sh/prebuilts/zImage
 
 # ANT+
